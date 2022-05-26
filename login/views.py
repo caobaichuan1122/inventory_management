dcc048dca1d4028f0cdf069c89a5054abe4e8787
Ansen <caobaichuan1122@gmail.com>
2022-05-26 14:33:33 +1000

    version 11

--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .froms import UserForm
-from login import models
+from . import models
 
 
 def index(request):
@@ -8,24 +8,31 @@ def index(request):
     return render(request,'login/index.html')
 
 def login(request):
+    # if request.session.get('is_login',None):
+    #     return redirect('/control/') #sing in page
+
     if request.method == "POST":
         login_form = UserForm(request.POST)
-        message = "Please check！"
+        message = "please check！"
         if login_form.is_valid():
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
-                    return redirect('/index/')
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
+                    return redirect('/index/')  #sing in page
                 else:
                     message = "password error！"
             except:
-                message = "account error！"
+                message = "user error！"
         return render(request, 'login/login.html', locals())
 
     login_form = UserForm()
     return render(request, 'login/login.html', locals())
+
 
 def register(request):
     pass
@@ -34,3 +41,7 @@ def register(request):
 def logout(request):
     pass
     return redirect('/index/')
+
+def control(request):
+    pass
+    return redirect('/control/control.html')
