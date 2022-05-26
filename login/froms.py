46696005bbc61d84006514f1be257f8f773a6bb0
Ansen <caobaichuan1122@gmail.com>
2022-05-26 11:22:56 +1000

    version 10

---
+++ login/froms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(label="user", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
