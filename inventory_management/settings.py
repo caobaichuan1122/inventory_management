acc532c0dd5b475ceb4c14715e58025f92f66d0d
Ansen <caobaichuan1122@gmail.com>
2022-06-02 15:10:55 +1000

    add public IP and port 49.176.154.111:8999

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -138,3 +138,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['49.176.154.111', 'localhost', '127.0.0.1']
