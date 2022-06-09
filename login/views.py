869ed6f60f49ffee094af7ac1357256754fc7c81
yangyj995 <yangyj9618@gmail.com>
2022-06-10 09:49:17 +1000

    update index.html

--- login/views.py
+++ login/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,redirect
-from .froms import UserForm,AddNewProduct,ModifyFix
+from .froms import UserForm,AddNewProduct,Modify_fix
 from . import models
 from .models import Trproduct, Tproduct, Prproduct, fix_tr_report, fix_tp_report
 
@@ -56,7 +56,10 @@ def index(request):
     Prdatabase = Prproduct.objects.all()
     FixTrDatabase = fix_tr_report.objects.all()
     FixTpDatabase = fix_tp_report.objects.all()
-    return render(request,'login/index.html',context={'Trdatabase': Trdatabase ,'Tdatabase':Tdatabase,'Prdatabase':Prdatabase,'FixTrDatabase':FixTrDatabase,'FixTpDatabase':FixTpDatabase})
+    edit_form_fix = Modify_fix()
+    return render(request, 'login/index.html',context={'Trdatabase': Trdatabase, 'Tdatabase': Tdatabase, 'Prdatabase': Prdatabase,
+                           'FixTrDatabase': FixTrDatabase, 'FixTpDatabase': FixTpDatabase,
+                           'edit_form_fix': edit_form_fix})
 
 def add_product(request):
     # if request.method == 'POST':
@@ -70,7 +73,7 @@ def add_product(request):
     # else:
     #     return render(request, '/login/index.html')
     product_form = AddNewProduct()
-    return render(request,'login/addnewproduct.html')
+    return render(request,'login/addnewproduct.html', locals())
 
 def del_fix_tr_product(request,id):
     models.fix_tr_report.objects.filter(id=id).delete()
@@ -94,6 +97,7 @@ def modify_product(request,id):
     else:
         return render(request, '/login/index.html')
 
-def edit_fix(request,id):
-    edit_form_fix = ModifyFix()
-    return render(request,'/login/index.html')
+def edit_fix(request):
+    # edit_form = AddNewProduct(request.POST)
+    edit_form_fix = Modify_fix()
+    return render(request,'/login/index.html',context={'edit_form_fix': edit_form_fix})
